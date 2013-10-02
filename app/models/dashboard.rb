3c6cefe2833f5aee9b9b7a9e8e60d410e92b0b4f
Dan Tashman <dantashman@gmail.com>
2013-10-02 15:02:49 -0400

    added contact/about/FAQ's
    added static pages

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -6,8 +6,6 @@ class Dashboard < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
-end
 
 
 
